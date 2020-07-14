cf9d404a4ac6b9b9786c67eba4f8b1256f0eb875
James Junaidi <james.junaidi@gmail.com>
2020-07-14 15:13:09 -0700

    second commit

---
+++ build/precache-manifest.83d89e88a3c53093927687d5c6ccdf05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93ba54c8714a4c7328eaf25a927d9bb",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "50c2fbe0be689f7be345",
+    "url": "/tic-tac-toe-react/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "95e640a7871147da1d23",
+    "url": "/tic-tac-toe-react/static/js/2.4027ae43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-react/static/js/2.4027ae43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c2fbe0be689f7be345",
+    "url": "/tic-tac-toe-react/static/js/main.a4207024.chunk.js"
+  },
+  {
+    "revision": "9059e9c74bc0af422287",
+    "url": "/tic-tac-toe-react/static/js/runtime-main.e6c5e9fe.js"
+  }
+]);
